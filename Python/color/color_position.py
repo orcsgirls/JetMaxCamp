8527a7f34ff36727260d7f191c36059cf93adff5
tproffen <thomasproffen@gmail.com>
2024-06-09 12:19:47 -0400

    Final changes

--- Python/color/color_position.py
+++ Python/color/color_position.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import cv2
 import math
@@ -5,9 +6,17 @@ import threading
 import numpy as np
 import rospy
 from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
+from std_srvs.srv import Empty
+from std_srvs.srv import SetBool, SetBoolResponse, SetBoolRequest
+from std_srvs.srv import Trigger, TriggerResponse, TriggerRequest
+from color_sorting.srv import SetTarget, SetTargetResponse, SetTargetRequest
+from std_msgs.msg import Bool
+from jetmax_control.msg import SetServo
 import hiwonder
 
-ROS_NODE_NAME = "color_angle"
+ROS_NODE_NAME = "color_pos"
+IMAGE_PROC_SIZE = 640, 480
 unit_block_corners = np.asarray([[0, 0, 0],
                                  [20, -20, 0],  # TAG_SIZE = 33.30mm
                                  [-20, -20, 0],
@@ -15,6 +24,36 @@ unit_block_corners = np.asarray([[0, 0, 0],
                                  [20, 20, 0]],
                                 dtype=np.float64)
 unit_block_img_pts = None
+
+
+def camera_to_world(cam_mtx, r, t, img_points):
+    inv_k = np.asmatrix(cam_mtx).I
+    r_mat = np.zeros((3, 3), dtype=np.float64)
+    cv2.Rodrigues(r, r_mat)
+    # invR * T
+    inv_r = np.asmatrix(r_mat).I  # 3*3
+    transPlaneToCam = np.dot(inv_r, np.asmatrix(t))  # 3*3 dot 3*1 = 3*1
+    world_pt = []
+    coords = np.zeros((3, 1), dtype=np.float64)
+    for img_pt in img_points:
+        coords[0][0] = img_pt[0][0]
+        coords[1][0] = img_pt[0][1]
+        coords[2][0] = 1.0
+        worldPtCam = np.dot(inv_k, coords)  # 3*3 dot 3*1 = 3*1
+        # [x,y,1] * invR
+        worldPtPlane = np.dot(inv_r, worldPtCam)  # 3*3 dot 3*1 = 3*1
+        # zc
+        scale = transPlaneToCam[2][0] / worldPtPlane[2][0]
+        # zc * [x,y,1] * invR
+        scale_worldPtPlane = np.multiply(scale, worldPtPlane)
+        # [X,Y,Z]=zc*[x,y,1]*invR - invR*T
+        worldPtPlaneReproject = np.asmatrix(scale_worldPtPlane) - np.asmatrix(transPlaneToCam)  # 3*1 dot 1*3 = 3*3
+        pt = np.zeros((3, 1), dtype=np.float64)
+        pt[0][0] = worldPtPlaneReproject[0][0]
+        pt[1][0] = worldPtPlaneReproject[1][0]
+        pt[2][0] = 0
+        world_pt.append(pt.T.tolist())
+    return world_pt
 
 
 class State:
@@ -42,6 +81,30 @@ class State:
             self.WIDTH = math.sqrt((l_p1[0] - l_p2[0]) ** 2 + (l_p1[1] - l_p2[1]) ** 2)
             print(unit_block_img_pts)
 
+
+
+def point_xy(pt_a, pt_b, r):
+    x_a, y_a = pt_a
+    x_b, y_b = pt_b
+    if x_a == x_b:
+        return x_a, y_a + (r / abs((y_b - y_a))) * (y_b - y_a)
+    k = (y_a - y_b) / (x_a - x_b)
+    b = y_a - k * x_a
+    A = k ** 2 + 1
+    B = 2 * ((b - y_a) * k - x_a)
+    C = (b - y_a) ** 2 + x_a ** 2 - r ** 2
+    x1 = (-B + math.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+    x2 = (-B - math.sqrt(B ** 2 - 4 * A * C)) / (2 * A)
+    y1 = k * x1 + b
+    y2 = k * x2 + b
+    dist_1 = math.sqrt((x1 - x_b) ** 2 + (y1 - y_b) ** 2)
+    dist_2 = math.sqrt((x2 - x_b) ** 2 + (y2 - y_b) ** 2)
+    if dist_1 <= dist_2:
+        return x1, y1
+    else:
+        return x2, y2
+
+
 def image_proc(img):
     img_h, img_w = img.shape[:2]
     frame_gb = cv2.GaussianBlur(np.copy(img), (5, 5), 5)
@@ -62,19 +125,43 @@ def image_proc(img):
                 box = cv2.boxPoints(rect)  # The four vertices of the minimum-area-rectangle
                 box_list = box.tolist()
                 box = np.int0(box)
+                ap = max(box_list, key=lambda p: math.sqrt((p[0] - state.K[0][2]) ** 2 + (p[1] - state.K[1][2]) ** 2))
+                index_ap = box_list.index(ap)
+                p1 = box_list[index_ap - 1 if index_ap - 1 >= 0 else 3]
+                p2 = box_list[index_ap + 1 if index_ap + 1 <= 3 else 0]
+                n_p1 = point_xy(ap, p1, state.WIDTH)
+                n_p2 = point_xy(ap, p2, state.WIDTH)
 
+                c_x, c_y = None, None
+                if n_p1 and n_p2:
+                    x_1, y_1 = n_p1
+                    x_2, y_2 = n_p2
+                    c_x = (x_1 + x_2) / 2
+                    c_y = (y_1 + y_2) / 2
+                    cv2.circle(img, (int(n_p1[0]), int(n_p1[1])), 2, (255, 255, 0), 10)
+                    cv2.circle(img, (int(n_p2[0]), int(n_p2[1])), 2, (255, 255, 0), 10)
+                    cv2.circle(img, (int(c_x), int(c_y)), 2, (0, 0, 0), 10)
+
+                cv2.circle(img, (int(ap[0]), int(ap[1])), 2, (0, 255, 255), 10)
                 cv2.drawContours(img, [box], -1, hiwonder.COLORS[color_name.upper()], 2)
                 cv2.circle(img, (int(center_x), int(center_y)), 1, hiwonder.COLORS[color_name.upper()], 5)
-                angle = rect[2]
-                angle = angle - 90 if angle > 45 else angle
-                s = "Angle:{:0.2f}deg".format(angle)
-                cv2.putText(img, s, (int(center_x), int(center_y)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+                rect = list(rect)
+                if c_x:
+                    rect[0] = c_x, c_y
+                else:
+                    rect[0] = (center_x, center_y)
+                x, y = rect[0]
+                cv2.circle(img, (int(x), int(y)), 1, (255, 255, 255), 5)
+                real_x, real_y, _ = camera_to_world(state.K, state.R, state.T, np.array((x, y)).reshape((1, 1, 2)))[0][0]
+                s = "{} x:{:0.2f} y:{:0.2f}".format(color_name.upper(), real_x, real_y)
                 print(s)
+                cv2.putText(img, s, (int(center_x), int(center_y)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
 
     cv2.line(img, (int(img_w / 2 - 10), int(img_h / 2)), (int(img_w / 2 + 10), int(img_h / 2)), (0, 255, 255), 2)
     cv2.line(img, (int(img_w / 2), int(img_h / 2 - 10)), (int(img_w / 2), int(img_h / 2 + 10)), (0, 255, 255), 2)
 
     return img
+
 
 def image_callback(ros_image):
     image = np.ndarray(shape=(ros_image.height, ros_image.width, 3), dtype=np.uint8, buffer=ros_image.data)
@@ -98,8 +185,6 @@ if __name__ == '__main__':
     state.target_colors = rospy.get_param('/lab_config_manager/color_range_list', {})
     del[state.target_colors['white']]
     del[state.target_colors['black']]
-    del[state.target_colors['ball']]
-    del[state.target_colors['tennis']]
     image_sub = rospy.Subscriber('/usb_cam/image_rect_color', Image, image_callback)
     try:
         rospy.spin()
