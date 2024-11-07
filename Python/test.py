27f648211d6118caa5c91104ce1be8b94174ec50
tproffen <thomasproffen@gmail.com>
2024-11-06 21:28:26 -0500

    Cleaned up examples and moved to new api

---
+++ Python/test.py
@@ -0,0 +1,23 @@
+from myjetmax import *
+
+jetmax = myJetMax()
+
+# This moves to the given coordinates x,y and height z as on the grid.
+# Units are mm - here we make a square
+# The last number is the duration for the move - here 1 second
+
+jetmax.move_to( 0, 0,30, 1)
+jetmax.move_to(80, 0,30, 1)
+jetmax.move_to(80,80,30, 1)
+jetmax.move_to( 0,80,30, 1)
+jetmax.move_to( 0, 0,30, 1)
+
+# This moves relative to the last position 
+# Here 40 mm up.
+
+jetmax.move_to( 0, 0,40, 1,relative=True)
+
+# This returns the current location of the sucker
+(cx,cy,cz) = jetmax.location()
+
+
