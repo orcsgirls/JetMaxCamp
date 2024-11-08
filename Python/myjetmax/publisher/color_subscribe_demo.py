3a976604de6255df14b5d02b1ae01847cb93f68e
tproffen <thomasproffen@gmail.com>
2024-11-07 21:55:24 -0500

    Started camera integration

---
+++ Python/myjetmax/publisher/color_subscribe_demo.py
@@ -0,0 +1,18 @@
+import rospy
+import json
+from std_msgs.msg import String
+ 
+color_data = {"color":"", "x":0, "y":0}
+
+def callback(data):
+    color_data = json.loads(data.data)
+    print(color_data[0]['color'])
+
+rospy.init_node('color_subscriber')
+rospy.Subscriber("color_location", String, callback)
+print ("Waiting for messages ..")
+
+try:
+    rospy.spin()
+except KeyboardInterrupt:
+    sys.exit(0)
