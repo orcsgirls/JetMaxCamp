5591b4bdf97690d610a260b82e442a20f158ecff
Thomas Proffen <thomasproffen@gmail.com>
2025-07-28 17:58:44 -0400

    Update test.py

--- Python/mqtt/test.py
+++ Python/mqtt/test.py
@@ -1,34 +1,30 @@
 import paho.mqtt.client as mqtt
 import time
 
-def on_connect(mqttc, obj, flags, reason_code, properties):
+def on_connect(mqttc, obj, flags, reason_code):
     print("reason_code: " + str(reason_code))
 
-def on_disconnect(mqttc, obj, flags, reason_code, properties):
+def on_disconnect(mqttc, obj, reason_code):
     print("reason_code: " + str(reason_code))
 
 def on_message(mqttc, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
-def on_subscribe(mqttc, obj, mid, reason_code_list, properties):
-    print("Subscribed: " + str(mid) + " " + str(reason_code_list))
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid))
 
-def on_unsubscribe(mqttc, obj, mid, reason_code_list, properties):
-    print("Unsubscribed: " + str(mid) + " " + str(reason_code_list))
-
-
-def on_log(mqttc, obj, level, string):
-    print(string)
+def on_unsubscribe(mqttc, obj, mid):
+    print("Unsubscribed: " + str(mid))
 
 
-mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc = mqtt.Client()
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 mqttc.on_unsubscribe = on_unsubscribe
 mqttc.on_disconnect = on_disconnect
 
-mqttc.connect("192.168.37.173", 1883, 60)
+mqttc.connect("192.168.37.111", 1883, 60)
 mqtt_topic = "control/conveyor"
 mqttc.subscribe(mqtt_topic)
 
