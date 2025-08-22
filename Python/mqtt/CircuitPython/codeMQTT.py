f8cc8a677d45dd6ad477a1231640285ab5d38664
Thomas Proffen <thomasproffen@gmail.com>
2025-08-21 20:55:23 -0400

    Updates

--- Python/mqtt/CircuitPython/codeMQTT.py
+++ Python/mqtt/CircuitPython/codeMQTT.py
@@ -3,83 +3,47 @@ import wifi
 import socketpool
 import board
 import digitalio
-import busio
-import pwmio
-import supervisor
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
-from rgb1602 import Screen
 from os import getenv
 
 #---------------------------------------------------------------------------------
 # Settings
 #---------------------------------------------------------------------------------
 
-ssid = getenv("CIRCUITPY_WIFI_SSID")
-password = getenv("CIRCUITPY_WIFI_PASSWORD")
-device_id = getenv("DEVICE_ID")
-
 mqtt_broker = "192.168.37.111"
-mqtt_topic_action = device_id+"/action"
-mqtt_topic_config = device_id+"/config"
-mqtt_topic_running = device_id+"/running"
+mqtt_topic = "control/conveyor"
 
-
-#---------------------------------------------------------------------------------
-
-# On board LED
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-# Display
-i2c = busio.I2C(board.GP13, board.GP12)
-screen = Screen(i2c)
-
-# Motor controller
 conveyor = digitalio.DigitalInOut(board.GP1)
 conveyor.direction = digitalio.Direction.OUTPUT
-
-#---------------------------------------------------------------------------------
-# LCD wrapper
-#---------------------------------------------------------------------------------
-
-def screen_update(message, color):
-    try:
-        screen.set_css_colour(color)
-        screen.update(f"{wifi.radio.ipv4_address}", message)
-    except:
-        pass
 
 #---------------------------------------------------------------------------------
 # MQTT Callback
 #---------------------------------------------------------------------------------
 
 def message(client, topic, message):
-
-    # Action commands
-    if(topic == mqtt_topic_action):
+    if(topic == mqtt_topic):
         if(message == 'start'):
-            screen_update(f"{device_id}: ON", "red")
+            print("Starting conveyor")
             led.value = True
             conveyor.value = True
-            mqtt_client.publish(mqtt_topic_running, "True")
         elif(message == 'stop'):
-            screen_update(f"{device_id}: OFF", "yellow")
+            print("Stopping conveyor")
             led.value = False
             conveyor.value = False
-            mqtt_client.publish(mqtt_topic_running, "False")
-
-    # Config commands
-    elif(topic == mqtt_topic_config):
-        if(message == 'reset'):
-            screen_update("RESET", "yellow")
-            supervisor.reload()
+        else:
+            print(f"ERROR: Invalid message {message}")
 
 #---------------------------------------------------------------------------------
 # Connecting to WiFi
 #---------------------------------------------------------------------------------
 
-screen_update("Starting ..", "WiFi")
+ssid = getenv("CIRCUITPY_WIFI_SSID")
+password = getenv("CIRCUITPY_WIFI_PASSWORD")
 
+print(f"Connecting to WiFi {ssid}")
 try:
     wifi.radio.connect(ssid, password)
     pool = socketpool.SocketPool(wifi.radio)
@@ -88,12 +52,9 @@ except TypeError:
     print(f"Could not connect to WiFi {ssid}")
     raise
 
-
 #---------------------------------------------------------------------------------
 # Setting up MQTT client
 #---------------------------------------------------------------------------------
-
-screen_update("Starting ..", "MQTT")
 
 mqtt_client = MQTT.MQTT(broker=mqtt_broker, port=1883, socket_pool=pool)
 
@@ -101,14 +62,8 @@ mqtt_client = MQTT.MQTT(broker=mqtt_broker, port=1883, socket_pool=pool)
 mqtt_client.on_message = message
 
 # Connect and subscribe to topic
-try:
-    mqtt_client.connect()
-    mqtt_client.subscribe(mqtt_topic_action)
-    mqtt_client.subscribe(mqtt_topic_config)
-    screen_update(f"{device_id}: OFF", "yellow")
-except:
-    screen_update("MQTT Error!!", "red")
-    raise
+mqtt_client.connect()
+mqtt_client.subscribe(mqtt_topic)
 
 #---------------------------------------------------------------------------------
 # Main loop
@@ -120,4 +75,3 @@ try:
 except KeyboardInterrupt:
     mqtt_client.unsubscribe(mqtt_topic)
     mqtt_client.disconnect()
-    screen.set_css_colour("black")
--- Python/mqtt/client_test.py
+++ Python/mqtt/client_test.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import time
 
+running = True
+
 def on_connect(mqttc, obj, flags, reason_code):
     print("reason_code: " + str(reason_code))
 
@@ -16,26 +18,38 @@ def on_subscribe(mqttc, obj, mid, granted_qos):
 def on_unsubscribe(mqttc, obj, mid):
     print("Unsubscribed: " + str(mid))
 
+def on_message(mqttc, userdata, message):
+    global running
+
+    if message.topic == mqtt_topic_running:
+        running = (message.payload == b'True')
+
 device_id = "Conveyor01"
 
 mqttc = mqtt.Client()
 mqttc.on_message = on_message
 mqttc.on_connect = on_connect
+mqttc.on_message = on_message
 mqttc.on_disconnect = on_disconnect
 
 mqttc.connect("192.168.37.111", 1883, 60)
-mqtt_topic = "control/"+device_id
+mqtt_topic_action = device_id+"/action"
+mqtt_topic_config = device_id+"/config"
+mqtt_topic_running = device_id+"/running"
+
+mqttc.subscribe(mqtt_topic_running)
 
 while True:
-    command = input(f"Enter command (r,s,x,q) :")
+    mqttc.loop()
+    command = input("Command (r,s,?,x,q):")
     if command == 'r':
-        print(f"Starting {device_id}")
-        mqttc.publish(mqtt_topic,"start")
+        mqttc.publish(mqtt_topic_action,"start")
     elif command == 's':
-        print(f"Stopping {device_id}")
-        mqttc.publish(mqtt_topic,"stop")
+        mqttc.publish(mqtt_topic_action,"stop")
+    elif command == '?':
+        print (f"{device_id} running - {running}")
     elif command == 'x':
-        mqttc.publish(mqtt_topic,"reset")
+        mqttc.publish(mqtt_topic_config,"reset")
     elif command == 'q':
         break
 
